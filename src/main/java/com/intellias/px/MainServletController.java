c323c636d243fc96634ab49c2067057e883ea34e
Liubomyr Pylyp <lpylyp@intellias.com>
2019-12-13 08:19:54 +0200

    First commit

---
+++ src/main/java/com/intellias/px/MainServletController.java
@@ -0,0 +1,17 @@
+package com.intellias.px;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class MainServletController extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        System.out.println(req.getContextPath());
+        super.doGet(req, resp);
+    }
+}
+
